229ad8bdd5b7943a3a6e0ae5c5fd766fcbbdbfa7
BadMilo11 <127443309+BadMilo11@users.noreply.github.com>
2026-03-09 20:05:12 -0600

    Remove unnecessary import and clean up code

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import streamlit as st
-
-# La siguiente línea de código es equivalente a print()
-st.write("# Hola Mundo")
 # Sección de importación de módulos
 from Modules.UI.header import show_header
 
