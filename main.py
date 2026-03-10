e81e2edb674057139390527670762ff87592c6aa
BadMilo11 <127443309+BadMilo11@users.noreply.github.com>
2026-03-09 21:07:42 -0600

    Implement sidebar navigation and map rendering
    Added sidebar navigation for visualizations and integrated map rendering functions.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 # Sección de importación de módulos
 from Modules.UI.header import show_header
 from Modules.Data.ecobici_service import cargar_estaciones_ecobici
+from Modules.Viz.ecobici_viz import renderizar_mapa_total, renderizar_detalle_estacion
 
 # Sección para crear la GUI
 show_header("Mi primera GUI en Streamlit")
@@ -18,3 +19,20 @@ if not df_estaciones.empty:
     
     # Ejemplo: Ver tabla
     st.dataframe(df_estaciones.head())
+    # 2. Sidebar para Navegación
+    st.sidebar.title("Navegación")
+    opcion = st.sidebar.radio(
+        "Selecciona una visualización:",
+        ["Mapa General", "Detalle de Estación"]
+    )
+
+    # 3. Lógica de despliegue
+    if opcion == "Mapa General":
+        st.title("🚲 Red Completa Ecobici")
+        renderizar_mapa_total(df_estaciones)
+        
+    elif opcion == "Detalle de Estación":
+        st.title("🔍 Análisis de Disponibilidad")
+        renderizar_detalle_estacion(df_estaciones)
+else:
+    st.error("No se pudieron cargar los datos de la API.")
