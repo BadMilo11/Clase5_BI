c7e310a95b6eeba99bb6097765dc4421d77a20fa
BadMilo11 <127443309+BadMilo11@users.noreply.github.com>
2026-03-09 20:04:30 -0600

    Add header import and display in Streamlit app

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 # La siguiente línea de código es equivalente a print()
 st.write("# Hola Mundo")
+# Sección de importación de módulos
+from Modules.UI.header import show_header
+
+# Sección para crear la GUI
+show_header("Mi primera GUI en Streamlit")
