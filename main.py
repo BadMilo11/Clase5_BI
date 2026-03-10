821ec72291844bbe9bb17b2b11aa21ee9217094e
BadMilo11 <127443309+BadMilo11@users.noreply.github.com>
2026-03-09 20:55:31 -0600

    Update data loading and display for Ecobici stations
    Replaced data loading function to use 'cargar_estaciones_ecobici' and added functionality to display active stations and a map.

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 # Sección de importación de módulos
 from Modules.UI.header import show_header
-from Modules.Data.ecobici_service import cargar_datos_ecobici
+from Modules.Data.ecobici_service import cargar_estaciones_ecobici
 
 # Sección para crear la GUI
 show_header("Mi primera GUI en Streamlit")
 
-# Obtienes el DataFrame ya limpio y listo
-df_ecobici = cargar_datos_ecobici()
+st.title("Monitoreo Ecobici CDMX")
 
-# Ahora puedes aplicar tu lógica de visualización o análisis
-print(df_ecobici['Dia_Semana'].value_counts())
-st.write(cargar_datos_ecobici())
+st.write(cargar_estaciones_ecobici())
+
+df_estaciones = cargar_estaciones_ecobici()
+
+if not df_estaciones.empty:
+    st.write(f"Mostrando {len(df_estaciones)} estaciones activas")
+    
+    # Ejemplo: Mapa de disponibilidad
+    st.map(df_estaciones, latitude='lat', longitude='lon')
+    
+    # Ejemplo: Ver tabla
+    st.dataframe(df_estaciones.head())
