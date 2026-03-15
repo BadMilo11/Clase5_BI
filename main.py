8094048941c34586f6a73da51a039046add1d13f
BadMilo11 <127443309+BadMilo11@users.noreply.github.com>
2026-03-15 11:19:04 -0600

    Refactor import and usage of PieBarChart functions

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ try:
     from Modules.UI.header import show_header
     from Modules.Data.ecobici_service import cargar_estaciones_ecobici
     from Modules.Viz.ecobici_viz import renderizar_mapa_total, renderizar_detalle_estacion
-    from Modules.Viz.PieBarChart import render_global_dashboard, render_station_comparison
+    import Modules.Viz.PieBarChart as pbc
 except Exception as e:
     st.error(f"Error crítico de importación: {e}")
     st.stop() # Detiene la app aquí para que leas el error
@@ -31,13 +31,13 @@ if not df_estaciones.empty:
     if opcion == "Mapa General":
         st.title("🚲 Red Completa Ecobici")
         render_global_dashboard(df)
-        renderizar_mapa_total(df_estaciones, zoom_seleccionado)
+        pbc.renderizar_mapa_total(df_estaciones, zoom_seleccionado)
         
     elif opcion == "Detalle de Estación":
         st.title("🔍 Análisis de Disponibilidad")
         id_sel = st.selectbox("Selecciona Estación", df['station_id'].values)
         # Para detalle, quizás queremos un zoom inicial más cercano
         renderizar_detalle_estacion(df_estaciones, zoom_seleccionado)
-        render_station_comparison(df, id_sel)
+        pbc.render_station_comparison(df, id_sel)
 else:
     st.error("No se pudieron cargar los datos.")
